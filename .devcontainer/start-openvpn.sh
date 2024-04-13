fcab359806bddca8c2150d0c2132e1e30d97b74d
Stefan Juhl <57107882+SJuhl75@users.noreply.github.com>
2024-04-13 18:45:45 +0000

    EN_VN

--- .devcontainer/start-openvpn.sh
+++ .devcontainer/start-openvpn.sh
@@ -19,3 +19,8 @@ fi
 
 # Start up the VPN client using the config stored in vpnconfig.ovpn by save-config.sh
 #nohup ${sudo_cmd} /bin/sh -c "openvpn --config vpnconfig.ovpn --log openvpn.log &" | tee openvpn-launch.log
+
+if [ "$EN_VPN" == "ON" ]; then
+    echo "ON" > ENVN.log
+fi
+echo $EN_VPN >> ENVN.LOG
